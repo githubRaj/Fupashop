a329e23e32301c92cf4e2cfd6ded0f5912896ccc
Arde <c_burns93@yahoo.ca>
2017-09-28 20:46:08 -0400

    added new template
    after login should redirected to home template

--- fupashop/resources/views/home.blade.php
+++ fupashop/resources/views/home.blade.php
@@ -1,23 +1,116 @@
-@extends('layouts.app')
+@extends('layout.main')
+
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+        <section class="hero text-center">
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br>
+            <a href=""><button class="button large">Start Buying</button></a>
+        </section>
+        <br/>
+        <div class="subheader text-center">
+             <h2>
+                Most Popular Products
+            </h2>
+        </div>
+       
+       
+        <div class="row">
+            <div class="small-3 columns">
+                <div class="item-wrapper">
+                    <div class="img-wrapper">
+                        <a class="button expanded add-to-cart">
+                            Add to Cart
+                        </a>
+                        <a href="#">
+                            <img src="{{ asset('dist/img/products/l1.jpg') }}"/>
+                        </a>
+                    </div>
+                    <a href="#">
+                        <h3>
+                            Hp specter bussines 
+                        </h3>
+                    </a>
+                    <h5>
+                        $19.99
+                    </h5>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin posuere sem enim, accumsan convallis risus semper.
+                    </p>
+                </div>
+            </div>
+            <div class="small-3 columns">
+                <div class="item-wrapper">
+                    <div class="img-wrapper">
+                        <a class="button expanded add-to-cart">
+                            Add to Cart
+                        </a>
+                        <a href="#">
+                            <img src="{{ asset('dist/img/products/tv1.jpg') }}"/>
+                        </a>
+                    </div>
+                    <a href="#">
+                        <h3>
+                           LG CURVED TV
+                        </h3>
+                    </a>
+                    <h5>
+                        $19.99
+                    </h5>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin posuere sem enim, accumsan convallis risus semper.
+                    </p>
+                </div>
+            </div>
+            <div class="small-3 columns">
+                <div class="item-wrapper">
+                    <div class="img-wrapper">
+                        <a class="button expanded add-to-cart">
+                            Add to Cart
+                        </a>
+                        <a href="#">
+                            <img src="{{ asset('dist/img/products/m1.jpg') }}"/>
+                        </a>
+                    </div>
+                    <a href="#">
+                        <h3>
+                            GAMING MONITOR
+                        </h3>
+                    </a>
+                    <h5>
+                        $19.99
+                    </h5>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin posuere sem enim, accumsan convallis risus semper.
+                    </p>
+                </div>
+            </div>
+            <div class="small-3 columns">
+                <div class="item-wrapper">
+                    <div class="img-wrapper">
+                        <a class="button expanded add-to-cart">
+                            Add to Cart
+                        </a>
+                        <a href="#">
+                            <img src="{{ asset('dist/img/products/c1.jpg') }}"/>
+                        </a>
+                    </div>
+                    <a href="#">
+                        <h3>
+                           IPHONE 20
+                        </h3>
+                    </a>
+                    <h5>
+                        $19.99
+                    </h5>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin posuere sem enim, accumsan convallis risus semper.
+                    </p>
                 </div>
             </div>
         </div>
-    </div>
-</div>
 @endsection
