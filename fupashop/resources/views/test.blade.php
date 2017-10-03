da821d5c6f689128148b4792a390e3ff485f8bb1
Arde <c_burns93@yahoo.ca>
2017-10-03 15:20:37 -0400

    test page
    ajusting testing page

--- fupashop/resources/views/test.blade.php
+++ fupashop/resources/views/test.blade.php
@@ -1,38 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>PC Part Picker</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
+@extends('layout.main')
+
+
+@section('content')
 
     <!-- Le styles 
     <link href="/static/css/bootstrap.css" rel="stylesheet">
     <link href="/static/css/bootstrap.css" rel="stylesheet">-->
     <link rel="stylesheet" href="{{ asset('/static/css/bootstrap.css') }}"/>
-    <style type="text/css">
-      body {
-        padding-top: 60px;
-        padding-bottom: 40px;
-      }
-      .sidebar-nav {
-        padding: 9px 0;
-      }
-
-      @media (max-width: 980px) {
-        /* Enable use of floated navbar text */
-        .navbar-text.pull-right {
-          float: none;
-          padding-left: 5px;
-          padding-right: 5px;
-        }
-      }
-      
-      footer{
-      margin-top: 2.5em;
-      text-align: center;
-      }
     </style>
     <link href="/static/css/bootstrap-responsive.css" rel="stylesheet">
     <script type="text/javascript" src={{ asset('/static/js/jquery.js') }}></script>
@@ -148,3 +122,4 @@
   <script type="text/javascript" src="/static/js/filter.js"></script>
   
 </html>
+@endsection
