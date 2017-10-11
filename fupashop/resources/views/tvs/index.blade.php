ba4c7b90932b91a895e59109264ff82612db358a
hamidehAziz <h_azizmohamadi@yahoo.com>
2017-10-11 10:29:24 -0400

    UI-tvs-add

--- fupashop/resources/views/tvs/index.blade.php
+++ fupashop/resources/views/tvs/index.blade.php
@@ -2,12 +2,167 @@
 
 
 @section('content')
+<!-- Le styles
+<link href="/static/css/bootstrap.css" rel="stylesheet">
+<link href="/static/css/bootstrap.css" rel="stylesheet">-->
+<link rel="stylesheet" href="{{ asset('/static/css/bootstrap.css') }}"/>
+</style>
+<link href="{{ asset('/static/css/bootstrap-responsive.css') }}" rel="stylesheet">
+<script type="text/javascript" src={{ asset('/static/js/jquery.js') }}></script>
+<script type="text/javascript" src={{ asset('/static/js/tablesorter.js') }}></script>
+<script type="text/javascript">
+	$(document).ready(function()
+	{
+
+	$("#myTable").tablesorter({sortList:[[0,0],[2,1]], widgets:'zebra'});
+	}
+	);
+</script>
+
+
+
+</head>
 
 <body>
-	@foreach ($tvs as $tv)
-		<li>{{ $tv->modelNumber }}</li>
-	@endforeach
-</body>
-</html>
 
+
+
+<div class="container-fluid">
+	<div class="row-fluid">
+		<div class="span3">
+			<div class="well sidebar-nav">
+				<!--<ul class="nav nav-list">
+					<li class="nav-header">Parts</li>
+					<li class="active"><a href="/cpu">CPUs</a></li>
+					<li><a href="/mobo">Motherboards</a></li>
+					<li><a href="/ram">RAM</a></li>
+					<li><a href="/hdd">Hard Drives</a></li>
+					<li><a href="/ssd">Solid State Drives</a></li>
+					<li><a href="/gpu">Graphics Cards</a></li>
+					<li><a href="/psu">Power Supplies</a></li>
+					<li><a href="/case">Cases</a></li>
+				</ul>-->
+	
+
+ <ul id="filters" class="nav nav-list">
+					<li class="nav-header">Brand</li>
+				 @php
+					$id=1
+				 @endphp
+
+
+					@foreach ($brand as $b) 
+	      			<li>&nbsp;&nbsp;&nbsp;<input type="checkbox" name="brand[]" value="{{ $b->brand }}" id="id000{{$id}}"/><font size="3">&nbsp;{{ $b->brand }}</font></li>
+	      			@php
+	      				$id = $id + 1 
+	      			@endphp
+	      	  @endforeach 
+
+	
+	</ul>
+	<ul id="filters" class="nav nav-list">
+					<li class="nav-header">Tv Type</li>
+				 @php
+					$id=1
+				 @endphp
+
+
+					@foreach ($tvType as $t) 
+	      			<li>&nbsp;&nbsp;&nbsp;<input type="checkbox" name="tvType[]" value="{{ $t->tvType }}" id="id000{{$id}}"/><font size="3">&nbsp;{{ $t->tvType }}</font></li>
+	      			@php
+	      				$id = $id + 1 
+	      			@endphp
+	      	  @endforeach 
+
+	
+	</ul>
+
+
+	<ul id="filters" class="nav nav-list">
+					<li class="nav-header">Resolution</li>
+				 @php
+					$id=1
+				 @endphp
+
+
+					@foreach ($resolution as $r) 
+	      			<li>&nbsp;&nbsp;&nbsp;<input type="checkbox" name="resolution[]" value="{{ $r->resolution }}" id="id000{{$id}}"/><font size="3">&nbsp;{{ $r->resolution }}</font></li>
+	      			@php
+	      				$id = $id + 1 
+	      			@endphp
+	      	  @endforeach
+	</ul>
+
+
+	<ul id="filters" class="nav nav-list">
+					<li class="nav-header">Screen Size</li>
+				 @php
+					$id=1
+				 @endphp
+
+
+					@foreach ($screenSize as $s) 
+	      			<li>&nbsp;&nbsp;&nbsp;<input type="checkbox" name="screenSize[]" value="{{ $s-> screenSize }}" id="id000{{$id}}"/><font size="3">&nbsp;{{ $s-> screenSize }}</font></li>
+	      			@php
+	      				$id = $id + 1 
+	      			@endphp
+	      	  @endforeach
+	</ul>
+
+	<ul class="nav nav-list">
+		<li class="nav-header">Price Range</li>
+		<input type="text" id="min" placeholder="Minimum"></input>
+		<input type="text" id="max" placeholder="Maximum"></input>
+		<button id="btnFilter">Find</button>
+	</ul>
+			</div><!--/.well -->
+		</div><!--/span-->
+		<div class="span9">
+<table id="myTable" class="tablesorter table table-hover">
+	<thead>
+			<tr>
+				<th>Model</th>
+				<th>Brand</th>
+				<th>TV Type</th>
+				<th>Resolution</th>
+				<th>Screen Size</th>
+				<th></th>
+				<th>Price</th>
+			</tr>
+		 </thead>
+
+			<tbody>
+		@foreach ($tvs as $tv)
+			<tr class="{{ $tv->resolution }}">
+					<tr>
+<td><a href="">{{ $tv->modelNumber }}</a></td>
+<td class="brand">{{ $tv->brand }}</td>
+<td>{{ $tv->tvType }}</td>
+<td>{{ $tv->resolution }}</td>
+<td>{{ $tv->screenSize }}</td>
+<td>{{ $tv->hddSize }}</td>
+<td class="price">{{ $tv->price }}</td>
+<td><a href="" class="btn btn-default btn-xs" role="button">BUY</a></td>
+		</tr>
+		@endforeach
+
+				</tbody>
+</table>
+		</div><!--/span-->
+	</div><!--/row-->
+
+	<hr>
+
+	<footer>
+		<p>&copy; JJTS</p>
+	</footer>
+
+</div><!--/.fluid-container-->
+
+
+</body>
+
+<script type="text/javascript" src="{{ asset('/static/js/filter.js') }}"></script>
+
+</html>
 @endsection
