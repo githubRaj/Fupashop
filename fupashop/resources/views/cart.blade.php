8f195c0ea131766b80323cb67ef74d54e7591137
Arde <c_burns93@yahoo.ca>
2017-10-27 12:47:04 -0400

    cart view in progress
    cart has no data yet

--- fupashop/resources/views/cart.blade.php
+++ fupashop/resources/views/cart.blade.php
@@ -9,6 +9,104 @@ if (!isset($_SESSION)) {
 @endphp
 
 @section('content')
+<head>
+  <meta charset="UTF-8">
+  <title>Shopping Cart</title>
+  <link href='https://fonts.googleapis.com/css?family=Lato:300,400' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="http://weloveiconfonts.com/api/?family=entypo">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+  <link rel='stylesheet prefetch' href='https://s3-us-west-2.amazonaws.com/s.cdpn.io/5175/utf-latest.min.css'>
+  <link rel="stylesheet" href="{{ asset('/assets/css/stylecart.css') }}">
+   
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
+
+</head>
+
+<body>
+  <div class="main">
+  <h1>Shopping cart</h1>
+  <h2 class="sub-heading">Free shipping from $100!</h2>
+
+  <section class="shopping-cart">
+    <ol class="ui-list shopping-cart--list" id="shopping-cart--list">
+
+      <script id="shopping-cart--list-item-template" type="text/template">
+        <li class="_grid shopping-cart--list-item">
+          <div class="_column product-image">
+            <img class="product-image--img" src="" alt="Item image" />
+          </div>
+          <div class="_column product-info">
+            <h4 class="product-name">product name here</h4>
+            <p class="product-desc">product description here</p>
+            <div class="price product-single-price">price here</div>
+          </div>
+          <div class="_column product-modifiers" data-product-price="priceHere">
+            <div class="_grid">
+              <button class="_btn _column product-subtract">&minus;</button>
+              <div class="_column product-qty">0</div>
+              <button class="_btn _column product-plus">&plus;</button>
+            </div>
+            <button class="_btn entypo-trash product-remove">Remove</button>
+            <div class="price product-total-price">$0.00</div>
+          </div>
+        </li>
+      </script>
+
+    </ol>
+
+    <footer class="_grid cart-totals">
+      <div class="_column subtotal" id="subtotalCtr">
+        <div class="cart-totals-key">Subtotal</div>
+        <div class="cart-totals-value">$0.00</div>
+      </div>
+      <div class="_column shipping" id="shippingCtr">
+        <div class="cart-totals-key">Shipping</div>
+        <div class="cart-totals-value">$0.00</div>
+      </div>
+      <div class="_column taxes" id="taxesCtr">
+        <div class="cart-totals-key">Taxes (6%)</div>
+        <div class="cart-totals-value">$0.00</div>
+      </div>
+      <div class="_column total" id="totalCtr">
+        <div class="cart-totals-key">Total</div>
+        <div class="cart-totals-value">$0.00</div>
+      </div>
+      <div class="_column checkout">
+        <button class="_btn checkout-btn entypo-forward">Checkout</button>
+      </div>
+    </footer>
+
+  </section>
+</div>
+  <script src='http://cdnjs.cloudflare.com/ajax/libs/zepto/1.0/zepto.min.js'></script>
+  <script  src="{{ asset('/assets/js/index.js') }}"></script>
+</body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--
 
 <link rel="stylesheet" href="{{ asset('/static/css/bootstrap.css') }}"/>
 </style>
@@ -76,6 +174,8 @@ if (!isset($_SESSION)) {
 <script type="text/javascript" src="{{ asset('/static/js/filter.js') }}"></script>
 
 </html>
+-->
+
 @endsection
 
 <!--
@@ -86,7 +186,7 @@ if (!isset($_SESSION)) {
   <link rel="stylesheet" href="http://weloveiconfonts.com/api/?family=entypo">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
   <link rel='stylesheet prefetch' href='https://s3-us-west-2.amazonaws.com/s.cdpn.io/5175/utf-latest.min.css'>
-  <link rel="stylesheet" href="{{ asset('/cart/css/style.css') }}">
+  <link rel="stylesheet" href="
    
   <script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
 
