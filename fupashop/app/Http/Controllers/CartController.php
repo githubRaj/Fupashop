69b819339206c2a2f25154b338b9db35ea534543
Nick Fong <nick.waterproof@gmail.com>
2017-10-27 20:57:43 -0400

    Cart Functionality II - Cart Totals
    Added functions to calculate subtotal, tax, and total of cart. Decided to store them in the Session so they can be accessed from the views. Fixed tablet product description pages according to Jeff's commit.

--- fupashop/app/Http/Controllers/CartController.php
+++ fupashop/app/Http/Controllers/CartController.php
@@ -19,6 +19,7 @@ use App\Mapper\Mapper;
 class CartController extends Controller
 {
     private $mapper;
+
     /**
      * Create a new controller instance.
      *
@@ -47,9 +48,7 @@ class CartController extends Controller
         $itemModelNum = $request->modelNumber;
         $item = $this->mapper->findItemByModelNumber( $itemModelNum, 'App\Items\Tablet' );
         Session::push('sessionCart', $item );
-
-        echo var_dump( session()->get('sessionCart'));
-
+        CartController::updateTotalsAndTax();
         return back()->with('addAlert', 'Item added successfully to cart!');
     }
 
@@ -59,10 +58,12 @@ class CartController extends Controller
         $itemModelNum = $request->modelNumberToDel;
         $localCart = Session::get('sessionCart');
         $i = 0;
-        foreach ($localCart as $item){
+        foreach ($localCart as $item)
+        {
           if ( $item )
           {
-            if ( $itemModelNum == $item->getModelNumber() ){
+            if ( $itemModelNum == $item->getModelNumber() )
+            {
               // Unset that particular index of local tmp cart
               unset($localCart[$i]);
               $updatedLocalCart = array_values($localCart);
@@ -75,11 +76,80 @@ class CartController extends Controller
             $i++;
           }
         }
+        CartController::updateTotalsAndTax();
         return back()->with('delAlert', 'Item deleted successfully from cart!');
     }
 
-    public function updateUserCart()
+    // Getter for cart grand total
+    public function getTotal()
     {
-
+      if ( null != ( session()->get('cartTotal') ) )
+      {
+        return session()->get('cartTotal');
+      }
+      return 0;
     }
+
+    // Getter for cart subtotal
+    public function getSubtotal()
+    {
+      if ( null != ( session()->get('cartSubtotal') ) )
+      {
+        return session()->get('cartSubtotal');
+      }
+      return 0;
+    }
+
+    // Calculate the 15% tax
+    public function calculateTax()
+    {
+      if ( null == ( session()->get('cartSubtotal') ) )
+      {
+        return 0;
+      }
+      //Round it down to 2 sig figs
+      return round( session()->get('cartSubtotal')
+                    * 0.15, 2, PHP_ROUND_HALF_DOWN) ;
+    }
+
+    // Update the cart subtotal
+    public function updateTotalsAndTax()
+    {
+      if ( null != ( session('sessionCart') ) )
+      {
+        $runningTotal = 0;
+        foreach( session('sessionCart') as $item )
+        {
+          $runningTotal = $runningTotal + $item->getPrice();
+        }
+      }
+      session()->put( 'cartSubtotal', $runningTotal);
+      session()->put( 'cartTax', CartController::calculateTax() );
+      session()->put( 'cartTotal', session()->get('cartSubtotal')
+                                           + session()->get('cartTax') );
+    }
+
+    // Retrieves an item from cart via its model number
+    public function getCartItem($modelNumber)
+    {
+      if ( null != ( session('sessionCart') ) )
+      {
+        foreach( session('sessionCart') as $item )
+        {
+          if ($item->getModelNumber() == $modelNumber)
+          {
+            return $item;
+          }
+        }
+      }
+      return null;
+    }
+
+    public function getSerialNumberOfItem( $item )
+    {
+      //To Be Added with Bonobo Chan's function
+      //Mockup below
+      //$serialNum = $this->mapper->findItemBySerialNumber( $item->getSerialNumber(), $item->getClassName() );
+    }
+
 }
