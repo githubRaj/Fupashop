2321b2fd42d94b27b23a0ee76260348a3f886250
githubRaj <raj34@live.ca>
2017-11-05 19:59:21 -0500

    Delete on model number triggers
    Each trigger needs to be copied separately into the database

---
+++ fupashop/triggersForDeletingModelNumbers.sql
@@ -0,0 +1,41 @@
+DROP TRIGGER IF EXISTS after_desktops_delete;
+DROP TRIGGER IF EXISTS after_laptops_delete;
+DROP TRIGGER IF EXISTS after_tablets_delete;
+DROP TRIGGER IF EXISTS after_monitors_delete;
+
+
+delimiter //
+CREATE TRIGGER after_desktops_delete
+AFTER DELETE ON desktops
+FOR EACH ROW
+BEGIN
+DELETE FROM serialNumbers where modelNumber = OLD.modelNumber;
+END;//
+delimiter;
+
+delimiter //
+CREATE TRIGGER after_laptops_delete
+AFTER DELETE ON laptops
+FOR EACH ROW
+BEGIN
+DELETE FROM serialNumbers where modelNumber = OLD.modelNumber;
+END;//
+delimiter;
+
+delimiter //
+CREATE TRIGGER after_tablets_delete
+AFTER DELETE ON tablets
+FOR EACH ROW
+BEGIN
+DELETE FROM serialNumbers where modelNumber = OLD.modelNumber;
+END;//
+delimiter;
+
+delimiter //
+CREATE TRIGGER after_monitors_delete
+AFTER DELETE ON monitors
+FOR EACH ROW
+BEGIN
+DELETE FROM serialNumbers where modelNumber = OLD.modelNumber;
+END;//
+delimiter;
