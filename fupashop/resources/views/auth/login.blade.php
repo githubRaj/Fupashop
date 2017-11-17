a04140093bc8133be4c262f49db9f2ef1225d197
Nick Fong <nick.waterproof@gmail.com>
2017-11-16 22:36:12 -0500

    UI-Stylized Login Page
    -Changelist
    	-Login page is nice now

--- fupashop/resources/views/auth/login.blade.php
+++ fupashop/resources/views/auth/login.blade.php
@@ -1,69 +1,50 @@
 @extends('layouts.app')
 
+<link href="{{ asset('css/login.css') }}" rel="stylesheet">
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
+<body>
+<div class="login">
+  <h1>Login</h1>
+                    <form method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                                <input id="email" type="email" name="email" placeholder="Email Address" value="{{ old('email') }}" required autofocus>
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
                                 @endif
-                            </div>
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
+                                <input id="password" type="password" name="password" placeholder="Password" required>
                                 @if ($errors->has('password'))
-                                    <span class="help-block">
                                         <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
                                 @endif
-                            </div>
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
                                 <div class="checkbox">
                                     <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                                        <input type="checkbox" name="remember" class="btn btn-link"{{ old('remember') ? 'checked' : '' }}> Remember Me
                                     </label>
                                 </div>
-                            </div>
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary btn-block btn-large">
                                     Login
                                 </button>
 
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                <a href="{{ route('password.request') }}">
                                     Forgot Your Password?
                                 </a>
-                            </div>
                         </div>
                     </form>
-                </div>
-            </div>
-        </div>
-    </div>
+
 </div>
+</body>
 @endsection
