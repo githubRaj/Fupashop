ee7d9635d08d120ea9285553d0dd6ef2256f1bf7
Nick Fong <nick.waterproof@gmail.com>
2017-11-24 16:22:16 -0500

    Purchase Rework to Accomodate Model

--- fupashop/app/Purchase.php
+++ fupashop/app/Purchase.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Purchase extends Model
 {
+    private $userID;
+    private $modelNumber;
+    private $serialNumber;
+    private $price;
+    private $created_at;
     /**
      * The attributes that are mass assignable.
      *
@@ -17,4 +22,49 @@ class Purchase extends Model
     protected $fillable = [
          'userID', 'modelNumber', 'serialNumber', 'price', 'created_at',
     ];
+
+    public void getUserId()
+    {
+      return $this->userId;
+    }
+
+    public void getModelNumber()
+    {
+      return $this->modelNumber;
+    }
+
+    public void getSerialNumber()
+    {
+      return $this->serialNumber;
+    }
+
+    public void getPrice()
+    {
+      return $this->price;
+    }
+
+    public void getCreatedAt()
+    {
+      return $this->created_at;
+    }
+
+    public void setModelNumber( $mn )
+    {
+      $this->modelNumber = $mn;
+    }
+
+    public void setSerialNumber( $sn )
+    {
+      $this->serialNumber = $sn;
+    }
+
+    public void setPrice( $nprice )
+    {
+      $this->price = $nprice;
+    }
+
+    public void setCreatedAt( $ca )
+    {
+      $this->created_at = $ca;
+    }
 }
